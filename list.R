c3ee90daf63ad4d0a7eca765cdec88a27de3cdf8
Christian Mongeau <chrMongeau@users.noreply.github.com>
2016-08-02 22:21:04 +0200

    Error checking on requests to IDEAS server

--- list.R
+++ list.R
@@ -15,6 +15,10 @@ reply_status <- 706873909484851200 # update in_reply_to_status_id
 
 source('keys.txt')
 
+# TODO: error checking on all requests.
+
+error <- FALSE
+
 #### OAuth key and secret #####
 twitter_token <-
 	oauth_app('twitter', key = oauth['key'], secret = oauth['secret']) %>%
@@ -23,10 +27,17 @@ twitter_token <-
 nicks_from_page <- function(u) {
 	Sys.sleep(2) # be nice
 
-	read_html(u) %>%
-		xml_find_all('//a[@class="twitter-follow-button"]') %>%
-		xml_attr('href') %>%
-		sub('.*twitter.com/', '', .)
+	html_page <- try(read_html(u))
+
+	if ( 'try-error' %in% class(html_page) ) {
+		error <- TRUE
+		stop('############ NO INTERNET ##############')
+	} else {
+		html_page %>%
+			xml_find_all('//a[@class="twitter-follow-button"]') %>%
+			xml_attr('href') %>%
+			sub('.*twitter.com/', '', .)
+	}
 }
 
 add_to_list <- function(user) {
@@ -37,7 +48,6 @@ add_to_list <- function(user) {
 		'&screen_name=', user) %>%
 	POST(config(token = twitter_token))
 }
-
 
 list_info <- paste0(root,
 		'show.json',
@@ -63,8 +73,6 @@ link2users <- 'https://ideas.repec.org/i/etwitter.html' %>%
 	xml_find_all('//table/tr/td/a') %>%
 	sub('<a href="([^\\"]+)\\".*', 'https://ideas.repec.org\\1', .)
 
-# TODO: handle error in the event of no internet connection
-#       (the scripts fails, but continues without saying so)
 N <- length(link2users)
 repec_all <- vector(length=N)
 for ( i in 1:N ) {
@@ -74,40 +82,44 @@ for ( i in 1:N ) {
 	flush.console()
 }
 
-to_add <- repec_all[!tolower(repec_all) %in% tolower(members) & !is.na(repec_all)]
+if ( error == FALSE ) {
+	to_add <- repec_all[!tolower(repec_all) %in% tolower(members) & !is.na(repec_all)]
 
-add_nick <- sapply(to_add, add_to_list)
+	add_nick <- sapply(to_add, add_to_list)
 
-## Remove user from list
-#paste0(root,
-#	'members/destroy.json',
-#	'?slug=', slug,
-#	'&owner_screen_name=', owner,
-#	'&screen_name=', 'SomeUser') %>%
-#	POST(config(token = twitter_token))
+	## Remove user from list
+	#paste0(root,
+	#	'members/destroy.json',
+	#	'?slug=', slug,
+	#	'&owner_screen_name=', owner,
+	#	'&screen_name=', 'SomeUser') %>%
+	#	POST(config(token = twitter_token))
 
-desc <- paste0('Unofficial list of economists on RePEc - ',
-		'https://ideas.repec.org/i/etwitter.html') %>%
-	url_escape
+	desc <- paste0('Unofficial list of economists on RePEc - ',
+			'https://ideas.repec.org/i/etwitter.html') %>%
+		url_escape
 
-# url_encode doesn't encode parentheses
-desc <- paste0(desc, '%20%28as%20on%20', Sys.Date(), '%29')
+	# url_encode doesn't encode parentheses
+	desc <- paste0(desc, '%20%28as%20on%20', Sys.Date(), '%29')
 
-update_list <- paste0(root,
-		'update.json',
-		'?slug=', slug,
-		'&owner_screen_name=', owner,
-		'&description=', desc) %>%
-	POST(config(token = twitter_token))
-#content(update_list)
+	update_list <- paste0(root,
+			'update.json',
+			'?slug=', slug,
+			'&owner_screen_name=', owner,
+			'&description=', desc) %>%
+		POST(config(token = twitter_token))
+	#content(update_list)
 
-updates <- paste(length(to_add), "#RePEc #economists have joined",
-		"https://ideas.repec.org/i/etwitter.html from", last_update, 'to',
-		Sys.Date(), 'List: https://twitter.com/chrMongeau/lists/repec-twitter') %>%
-	url_escape
+	updates <- paste(length(to_add), "#RePEc #economists have joined",
+			"https://ideas.repec.org/i/etwitter.html from", last_update, 'to',
+			Sys.Date(), 'List: https://twitter.com/chrMongeau/lists/repec-twitter') %>%
+		url_escape
 
-reply_update <-	paste0('https://api.twitter.com/1.1/statuses/update.json',
-		'?status=', updates,
-		'&in_reply_to_status_id=', reply_status) %>%
-	POST(config(token = twitter_token))
+	reply_update <-	paste0('https://api.twitter.com/1.1/statuses/update.json',
+			'?status=', updates,
+			'&in_reply_to_status_id=', reply_status) %>%
+		POST(config(token = twitter_token))
+} else {
+	print('Some error occurred.')
+}
 
